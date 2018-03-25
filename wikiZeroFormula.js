1d434dc2fd7503b19e219ab5fc14604ff7624522
tbrknt <berkantkepez@gmail.com>
2018-03-25 22:21:49 +0300

    Implemented console-ready javascript code, which makes formulas on wiki zero pages rendered.

---
+++ wikiZeroFormula.js
@@ -0,0 +1,41 @@
+function extractRedirectedUrl(text){
+	var elem = document.createElement("html");
+	elem.innerHTML = text;
+	var aElems = elem.getElementsByTagName("a");
+	var imgsrc = aElems[0].href;
+	/*
+	text = text.substring(text.indexOf("href=") + "href=".length);
+	var imgsrc = text.substring(0, text.indexOf(">"));
+	*/
+	return imgsrc;
+}
+
+function httpGetAsync(theUrl, callback)
+{
+    var xmlHttp = new XMLHttpRequest();
+    xmlHttp.onreadystatechange = function() { 
+        if (xmlHttp.readyState == 4 && xmlHttp.status == 500)
+            callback(xmlHttp.responseText);
+    }
+    xmlHttp.open("GET", theUrl, true); // true for asynchronous 
+    xmlHttp.send(null);
+}
+
+function httpGet(theUrl, mathElem)
+{
+	httpGetAsync(theUrl, function(responseText){
+		mathElemSrc = extractRedirectedUrl(responseText);
+		mathElem.src = mathElemSrc;
+	});
+}
+
+var mathElems = document.getElementsByClassName("mwe-math-fallback-image-inline");
+
+for(var i=0; i<mathElems.length; i++){
+	httpGet(mathElems[i].src, mathElems[i]);
+}
+
+
+var i=0;
+httpGet(mathElems[i].src, mathElems[i]);
+
